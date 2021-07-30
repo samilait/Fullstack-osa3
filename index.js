9a7380d280e6f67dd9617411e37db6d40f9250dd
samilait <sami.p.laitinen@gmail.com>
2021-07-30 22:03:20 +0300

    Tehtävät 3.19-3.21

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -97,7 +99,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
   // response.status(204).end()
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name) {
@@ -111,12 +113,12 @@ app.post('/api/persons', (request, response) => {
     })    
   }
 
-  const personExists = persons.find(person => person.name === body.name)
-  if (personExists) {
-    return response.status(400).json({
-      error: 'name must be unique'
-    })    
-  }
+  // const personExists = persons.find(person => person.name === body.name)
+  // if (personExists) {
+  //   return response.status(400).json({
+  //     error: 'name must be unique'
+  //   })    
+  // }
 
   const person = new Person({
     name: body.name,
@@ -127,6 +129,7 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 
   // persons = persons.concat(person)
 
